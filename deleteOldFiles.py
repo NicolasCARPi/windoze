f572a22a91f9c1e0c039894748b9f4c280ae279e
Nicolas CARPi <nicolas.carpi@curie.fr>
2017-09-21 16:11:21 +0200

    capital TIF

--- deleteOldFiles.py
+++ deleteOldFiles.py
@@ -15,6 +15,6 @@ for dirpath, dirnames, filenames in os.walk(workdir):
     for file in filenames:
         curpath = os.path.join(dirpath, file)
         file_modified = datetime.datetime.fromtimestamp(os.path.getmtime(curpath))
-        if datetime.datetime.now() - file_modified > datetime.timedelta(days=olderThanDays) and curpath.endswith('.tif'):
+        if datetime.datetime.now() - file_modified > datetime.timedelta(days=olderThanDays) and curpath.endswith('.TIF'):
             os.unlink(curpath)
             print("removed {}".format(curpath))
